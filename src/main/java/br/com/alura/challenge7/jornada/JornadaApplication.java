9a5869daa9d8b8de2d6ae3201bd8df2499fc9b8b
Victor Tavares <ti@assempbh.org.br>
2023-07-23 19:08:13 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/alura/challenge7/jornada/JornadaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenge7.jornada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/challenge7/jornada/JornadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenge7.jornada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
